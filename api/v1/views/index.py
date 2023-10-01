3d442b91858bf27b98a8e2ae43b1c1525323464d
Walterthesmart <nwaugowalter@gmail.com>
2023-10-01 12:29:44 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-
-'''api status'''
-
-import models
+"""
+This script contains endpoint(route) status
+"""
 from models import storage
-from models.base_model import BaseModel
-from flask import jsonify
+from flask import Flask
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
-    return jsonify(status='OK')
+def status():
+    """
+    Returns a JSON status
+    """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
